13565acf5366daba5a14ff96c63e6ea199e58ec1
vishnu-3001 <vishnusai3001@yahoo.com>
2025-06-20 20:46:35 -0400

    trying for deployment

--- portfolio/src/Components/Navigation.jsx
+++ portfolio/src/Components/Navigation.jsx
@@ -1,70 +1,45 @@
-import classes from "./Navigation.module.css"
-import { hoverColors } from "../Utils/colors"
-import { useState } from "react"
-export default function Navigation(){
-    const [hovered,setHovered]=useState('');
-    const name="<Vishnu sai Kotha/>"
-    function handleMouseEnter(buttonName){
-        setHovered(buttonName);
-    }
-    function handleMouseLeave(){
-        setHovered('');
-    }
+import classes from "./Navigation.module.css";
+import contact from "../Utils/Contact";
+
+export default function Navigation() {
+    const name = "<Vishnu sai Kotha/>";
+
     return (
         <div className={classes.navigationContainer}>
             <div>
                 <p className={classes.name}>{name}</p>
             </div>
-            {/* <div>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Abt')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Abt'?hoverColors[8]:'white',
-                    borderBottom:`2px solid ${hovered==='Abt'?hoverColors[8]:'transparent'}`}}
-                >About</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Exp')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Exp'?hoverColors[2]:'white',
-                    borderBottom:`2px solid ${hovered==='Exp'?hoverColors[2]:'transparent'}`}}
-                >Experience</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Edu')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Edu'?hoverColors[4]:'white',
-                    borderBottom:`2px solid ${hovered==='Edu'?hoverColors[4]:'transparent'}`}}
-                >Education</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Proj')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Proj'?hoverColors[0]:'white',
-                    borderBottom:`2px solid ${hovered==='Proj'?hoverColors[0]:'transparent'}`}}
-                >Projects</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('C&P')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='C&P'?hoverColors[7]:'white',
-                    borderBottom:`2px solid ${hovered==='C&P'?hoverColors[7]:'transparent'}`}}
-                >Achievements</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Lead')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Lead'?hoverColors[6]:'white',
-                    borderBottom:`2px solid ${hovered==='Lead'?hoverColors[6]:'transparent'}`}}
-                >Leadership</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Res')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Res'?hoverColors[1]:'white',
-                    borderBottom:`2px solid ${hovered==='Res'?hoverColors[1]:'transparent'}`}}
-                >Resume</button>
-                <button className={classes.navButton}
-                onMouseEnter={()=>handleMouseEnter('Cont')}
-                onMouseLeave={handleMouseLeave}
-                style={{color:hovered==='Cont'?hoverColors[9]:'white',
-                    borderBottom:`2px solid ${hovered==='Cont'?hoverColors[9]:'transparent'}`}}
-                >Contact</button>
-            </div> */}
+            <div className={classes.iconGroup}>
+                {contact.map((item, index) => {
+                    // Determine actual URL type
+                    const isPhone = item.alt === "mobile";
+                    const isEmail = item.alt === "email";
+
+                    // Construct href for phone or email
+                    let href = item.url;
+                    if (isPhone && item.text) href = `tel:${item.text.replace(/[-\s]/g, "")}`;
+                    if (isEmail && item.url) href = `mailto:${item.url}`;
+
+                    return (
+                        <a
+                            href={href}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            key={index}
+                            className={classes.contactLink}
+                        >
+                            <img
+                                src={item.img}
+                                alt={item.alt}
+                                className={classes.contactImage}
+                            />
+                            {isPhone && item.text && (
+                                <span className={classes.contactText}>{item.text}</span>
+                            )}
+                        </a>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
---
+++ portfolio/src/Utils/Contact.js
@@ -0,0 +1,31 @@
+import github from "../Utils/github.png";
+import linkedin from "../Utils/linkedin.png";
+import call from "../Utils/phone-call.png";
+import email from "../Utils/email.png";
+const contact=[
+    {
+        "img":github,
+        "alt":"github",
+        "url":"https://github.com/vishnu-3001",
+        "text":""
+    },
+    {
+        "img":linkedin,
+        "alt":"linkedin",
+        "url":"https://www.linkedin.com/in/vishnu-sai-kotha-081689170/",
+        "text":""
+    },
+    {
+        "img":call,
+        "alt":"mobile",
+        "url":"",
+        "text":"+1 934-263-1669"
+    },
+    {
+        "img":email,
+        "alt":"email",
+        "url":"vishnusai.kotha30@gmail.com",
+        "text":""
+    },
+]
+export default contact;
